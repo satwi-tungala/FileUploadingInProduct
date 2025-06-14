657291f19d2494e698767f43fee7d9ad6fe018b7
satwi-tungala <balasatwikatg59@gmail.com>
2025-06-14 19:45:42 +0530

    Updated ProductController by adding Inline Documentation

--- src/main/java/com/example/githubproduct/controller/ProductController.java
+++ src/main/java/com/example/githubproduct/controller/ProductController.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
 import com.example.githubproduct.model.ProductEntity;
 import com.example.githubproduct.service.ProductServices;
-@Controller
+@RestController
 @RequestMapping("products")
 public class ProductController {
     @Autowired
